bf6003efa7397a72076815bed4b31a9d05481ff2
Eugenio Arévalo <eugenio.arevalo@gmail.com>
2020-02-11 11:44:14 +0100

    Utils: constants to be used in the app Styles: CSS global variables (colours)

---
+++ src/app/shared/utils/constants.ts
@@ -0,0 +1,5 @@
+export const URL_SERVER = ' https://api.github.com';
+export const SEARCH = 'search';
+export const USERS = 'users';
+export const REPOS = 'repos';
+export const URL_GIF = 'https://user-images.githubusercontent.com/2184469/40949613-7542733a-6834-11e8-895b-ce1cc3af9dbb.gif';
